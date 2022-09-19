b5db51d5f9f20910b05d9077bcf1ca42dacdeb76
Bhooshan007 <bhooshans95@gmail.com>
2022-09-19 15:12:41 +0530

    Added:Link list program

---
+++ src/com/bridgelabz/LinkedList.java
@@ -0,0 +1,88 @@
+package com.bridgelabz;
+
+public class LinkedList 
+{
+	Node head, tail;
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null)
+            head = newNode;
+        else
+            tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.key + "->");
+            temp = temp.next;
+        }
+    }
+
+    public void push(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+    public void insertAfter(int prevData,int data){
+        Node newNode = new Node(data);
+        Node searchedNode = this.search(prevData);
+        if( searchedNode != null){
+            newNode.next = searchedNode.next;
+            searchedNode.next = newNode;
+            //newNode.next = tempNode;
+        }
+    }
+    public Node search(int data){
+        Node temp = head;
+        while(temp != null){
+            if(temp.key == data)
+                return temp;
+            temp = temp.next;
+        }
+        return null;
+    }
+    public void pop(){
+        head = head.next;
+    }
+    public void popLast(){
+        Node temp = head;
+        Node prevNode = null;
+        while (temp.next != null)
+        {
+            prevNode = temp;
+            temp = temp.next;
+        }
+        assert prevNode != null;
+        prevNode.next = null;
+    }
+    public void delete(int data){
+        Node temp = head;
+        Node prevNode = head;
+        while(temp.key != data) {
+            prevNode = temp;
+            temp = temp.next;
+        }
+        if (temp == head)
+            head = head.next;
+        else
+            prevNode.next = temp.next;
+    }
+    public void size(){
+        Node temp = head;
+        int size = 1;
+        while (temp.next != null){
+            size++;
+            temp = temp.next;
+        }
+        System.out.println("Size : "+size);
+    }
+
+}
---
+++ src/com/bridgelabz/MainClass.java
@@ -0,0 +1,25 @@
+package com.bridgelabz;
+
+public class MainClass 
+{
+	public static void main(String[] args)
+    {
+
+        SortedLinkedList slist = new SortedLinkedList();
+        slist.add(56);
+        slist.add(30);
+        slist.add(70);
+        slist.add(40);
+        //list.insertAfter(30,40);
+        slist.display();
+//        list.delete(40);
+//        list.display();
+//        list.size();
+//        Node searchedNode = list.search(40);
+//        if (searchedNode == null)
+//            System.out.println("\nData Not Found");
+//        else
+//            System.out.println("\nData Found");
+    }
+
+}
